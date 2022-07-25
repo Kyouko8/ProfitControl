a2595b83b96773d0ab26b2c1a62cb04742eb0d2c
unknown <lucamendez18@hotmail.com>
2022-07-24 22:27:58 -0300

    prod mode

--- app/__init__.py
+++ app/__init__.py
@@ -28,7 +28,7 @@ def configure_logging():
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(configuration.Development)
+    app.config.from_object(configuration.Production)
 
     login_manager.init_app(app)
     login_manager.login_view = "auth.login"
